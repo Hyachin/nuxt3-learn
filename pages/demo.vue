372af75789f7ea875238941ca916752e38c43ecc
糖糖 <1041861275@qq.com>
2022-08-21 17:34:08 +0800

    init

---
+++ pages/demo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="">
+    demo
+    <NuxtLink to="/">go to index</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+import {} from "vue";
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+    index
+
+    <NuxtLink to="/demo">go to demo</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+import {} from "vue";
+</script>
+
+<style scoped></style>
