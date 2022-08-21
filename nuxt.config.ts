7011fb885c9bb27cf6dab6d36a8d49091533dff2
糖糖 <1041861275@qq.com>
2022-08-21 22:35:45 +0800

    seo optimization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  meta: {
+    link: [
+      {
+        rel: 'shortcut icon external nofollow"',
+        type: "image/x-icon",
+        href: "/favicon.ico",
+      },
+    ],
+  },
+});
--- pages/index.vue
+++ pages/index.vue
@@ -19,7 +19,16 @@
 </template>
 
 <script setup>
-import {} from "vue";
+// 实际项目中每个页面都需要配置 根据页面内容不同替换不同的数据 以便搜索引擎检索
+useHead({
+  title: "Nuxt3快速入门-首页",
+  viewport: "width=device-width,initial-scale=1,maximum-scale=1",
+  charset: "utf-8",
+  meta: [
+    { name: "description", content: "Nuxt3快速入门代码指南" },
+    { name: "keywords", content: "Nuxt3" },
+  ],
+});
 </script>
 
 <style scoped></style>
