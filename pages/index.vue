0f958e2ec663af809761cf35e1f23db6b093346b
糖糖 <1041861275@qq.com>
2022-08-21 19:31:32 +0800

    nested routes

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,8 @@
     <NuxtLink to="/demo">go to demo</NuxtLink>
     <br />
     <NuxtLink to="/user/1">go to user with param id = 1</NuxtLink>
+    <br />
+    <NuxtLink to="/father/son/grandson">try nested routes</NuxtLink>
   </div>
 </template>
 
